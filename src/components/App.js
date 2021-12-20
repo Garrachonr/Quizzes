350d6bd340cf7b41e05c5736b06bce26bba6a8bf
Alfredo Garrachón <garrachonr@outlook.com>
2021-12-20 19:16:35 +0100

    1.01

--- src/components/App.js
+++ src/components/App.js
@@ -1,68 +1,50 @@
+import { setSelectionRange } from '@testing-library/user-event/dist/utils';
 import React, { useState, useEffect } from 'react';
-import Header from './Header.jsx';
-import Board from './Board.jsx';
-import Reset from './Reset.jsx';
+import { NavLink } from 'react-bootstrap';
+import Tictac from './Tictactoe.js';
+//Hay que hacer el diccionario
+// import es from "./lang/es.json";
 
-const PLAYERX = "Player 1 - Xs";
-const PLAYER0 = "Player 2 - 0s";
+export const LangContext = React.createContext({ userLang: "es", dictionary: es });
 
 export default function App(props) {
-  const [turn, setTurn] = useState(PLAYERX);
-  const [moves, setMoves] = useState(0);
-  const [values, setValues] = useState([
-    ['-', '-', '-'],
-    ['-', '-', '-'],
-    ['-', '-', '-']
-    ]);
-
-  useEffect(() => {
-    // Update the document title using the browser API
-    document.title = `Turn of ${turn}`;
-  });
-
-  useEffect(() => {
-    async function fetchData() {
-      const res = await fetch("http://myjson.dit.upm.es/api/bins/ccr5");
-      const myjson = await res.json();
-      console.log(myjson);
-      setTurn(myjson.turn);
-      setMoves(myjson.moves);
-      setValues(myjson.values);
-    }
-
-    fetchData();
-  }, []);
-
-  function appClick(rowNumber, columnNumber) {
-      let valuesCopy = JSON.parse(JSON.stringify(values));
-      let newMovement = turn === PLAYERX ? 'X' : '0';
-      valuesCopy[rowNumber][columnNumber] = newMovement;
-      setTurn(turn === PLAYERX ? PLAYER0 : PLAYERX);
-      setValues(valuesCopy);
-      setMoves(moves + 1); 
+  const [lang, setLang] = useState("en");
+  const handleLanguageChange = (event) => {
+    setLang(event.target.value);
   }
-
-  function resetClick(){
-    setTurn(PLAYERX);
-    setMoves(0);
-    setValues([
-      ['-', '-', '-'],
-      ['-', '-', '-'],
-      ['-', '-', '-']
-    ]);
-  }
-
-  
-  let text = "Turn of " + turn;
 
   return (
     <div>
-      <Header text={text}/>
-      <Board values={values}  appClick={appClick}/>
-      <h3>Number of moves: {moves}</h3>
-      <Reset resetClick={resetClick}></Reset>
+      <LangContext.Provider value={{ handleLanguageChange: handleLanguageChange, userLang: lang, dictionary: dictionaryList[lang] }}>
+        <LangContext.Consumer>
+          {(context) => {
+            return
+            <div>
+              <nav className='nav nav-tabs' roles="tablist">
+                <NavLink to="/">TicTacToe</NavLink>
+                <NavLink to="/tictactoe">TicTacToe</NavLink>
+                <NavLink to="/quiz">Quiz</NavLink>
+              </nav>
+            </div>
+          }}
+        </LangContext.Consumer>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="tictactoe" element={<TicTacToe />} />
+          <Route path="quiz" element={<Quiz />} />
+        </Routes>
+      </LangContext.Provider>
     </div>
   );
+
+
+
+
+
+
+
+
+
 
 
 }
